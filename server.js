00290fbea2861859cd92b036ff98bd9c42ca17b7
omur8819 <omur8819@gmail.com>
2020-12-18 15:47:52 +0300

    Add prodcution case

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+//production
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    });
+}
+
 
 const host = "http://localhost";
 const port = process.env.PORT;
